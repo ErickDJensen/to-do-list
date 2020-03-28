66874afe8a10fd8c5c465c2c6419c9aca100504e
Erick Jensen <Erick.Jensen010@gmail.com>
2020-03-28 13:07:46 -0500

    finish GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,26 @@ $(document).ready(handleReady);
 
 function handleReady(){
     console.log('jQuery ready to go!')
+    refreshTasks();
+}
+
+
+function refreshTasks() {
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        console.log('get tasks', response);
+        renderTasks(response);  
+    }).catch(function (error) {
+        console.log('Catch the error', error);
+    })
+}
+
+function renderTasks(tasks){
+    console.log('In renderLog'); 
+    $('#taskList').empty();
+    for (let item of tasks) {
+        $('#taskList').append(`<li>${item.task}</li>`)
+    }      
 }
--- server/server.js
+++ server/server.js
@@ -1,17 +1,49 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+const bodyParser = require('body-parser');
+const PORT = 5000;
+const pg = require('pg');
+const Pool = pg.Pool;
 
-
-
-// Serve back static files by default
-app.use(express.static('server/public'))
-
-// Start listening for requests on a specific port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+const pool = new Pool({
+    database: 'weekend-to-do-app', // Change to match your DB
+    host: 'localhost', // where your database is
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000 // 30 second timeout on idle queries
 });
+
+pool.on('connect', () => {
+    console.log('Database Connection established...');
+})
+
+pool.on('error', (error) => {
+    console.log('Database error:', error);
+})
+
+
+// use bodyParser.urlencoded throughout the app with this:
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// serve back static files
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log('server running on: ', PORT);
+}); // end spin up server
+
+app.get('/tasks', (req, res) => {
+    console.log('in /tasks GET');
+    let sqlText = `SELECT * FROM "weekendToDo";`;
+    pool.query(sqlText)
+        .then(result => {
+            console.log('Result: ', result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('Gon an error on Select query', error);
+            res.sendStatus(500);
+        })
+})
